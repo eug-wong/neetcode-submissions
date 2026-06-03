04ab0946363b8e560b1295ae38d5110b73b53ab2
Eugene Wong <110069795+eug-wong@users.noreply.github.com>
2026-06-03 19:02:25 +0800

    Add: longest-increasing-path-in-matrix - submission-1

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        def isValid(i, j):
+            return i >= 0 and i < len(matrix) and j >= 0 and j < len(matrix[0])
+
+        def dfs(i, j, length):
+            nonlocal memo
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            res = 1
+            if isValid(i + 1, j) and matrix[i + 1][j] > matrix[i][j]:
+                res = max(res, 1 + dfs(i + 1, j, length + 1))
+            
+            if isValid(i, j + 1) and matrix[i][j + 1] > matrix[i][j]:
+                res = max(res, 1 + dfs(i, j + 1, length + 1))
+            
+            if isValid(i - 1, j) and matrix[i - 1][j] > matrix[i][j]:
+                res = max(res, 1 + dfs(i - 1, j, length + 1))
+            
+            if isValid(i, j - 1) and matrix[i][j - 1] > matrix[i][j]:
+                res = max(res, 1 + dfs(i, j - 1, length + 1))
+            
+            memo[(i, j)] = res
+            return res
+        
+        memo = {}
+        ans = 0
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                ans = max(ans, dfs(i, j, 1))
+        
+        return ans
