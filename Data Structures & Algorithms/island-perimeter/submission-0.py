569f2dedbe172ef6fdc096c34c89614a4d7fa1b1
Eugene Wong <110069795+eug-wong@users.noreply.github.com>
2026-06-14 08:08:03 +0800

    Add: island-perimeter - submission-0

---
+++ Data Structures & Algorithms/island-perimeter/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        def dfs(i, j):
+            nonlocal visited
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0:
+                return 1
+            
+            if (i, j) in visited:
+                return 0
+
+            visited.add((i, j))
+            
+            return dfs(i - 1, j) + dfs(i + 1, j) + dfs(i, j - 1) + dfs(i, j + 1)
+        
+        visited = set()
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    return dfs(i, j)
+        
+        return 0
