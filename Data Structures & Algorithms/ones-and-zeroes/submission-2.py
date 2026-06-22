c480c5e8882472492e81a9790643ff99162da2ee
Eugene Wong <110069795+eug-wong@users.noreply.github.com>
2026-06-22 08:12:39 +0800

    Add: ones-and-zeroes - submission-2

---
+++ Data Structures & Algorithms/ones-and-zeroes/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        memo = {}
+        def recurse(i, m, n):
+            nonlocal memo
+            if i == len(strs):
+                return 0
+            
+            if m == 0 and n == 0:
+                return 0
+            
+            if (i, m, n) in memo:
+                return memo[(i, m, n)]
+            
+            freq = Counter(strs[i])
+
+            res = recurse(i + 1, m, n)
+            if m - freq["0"] >= 0 and n - freq["1"] >= 0:
+                res = max(res, 1 + recurse(i + 1, m - freq["0"], n - freq["1"]))
+            
+            memo[(i, m, n)] = res
+            
+            return res
+        
+        return recurse(0, m, n)
