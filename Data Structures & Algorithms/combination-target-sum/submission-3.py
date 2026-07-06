812f29078a1efc06bd80f2788e4a54161c70e810
Eugene Wong <110069795+eug-wong@users.noreply.github.com>
2026-07-06 17:36:11 -0500

    Add: combination-target-sum - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum/submission-3.py
@@ -0,0 +1,27 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = set()
+        def recurse(i, cur):
+            nonlocal res
+            if sum(cur) == target:
+                res.add(tuple(cur))
+                return
+
+            if i == len(nums) or sum(cur) > target:
+                return
+            
+            # accept
+            cur = cur + [nums[i]]
+            recurse(i, cur)
+            cur.pop()
+            # accept and move on
+            cur = cur + [nums[i]]
+            recurse(i + 1, cur)
+            cur.pop()
+            # reject and move on
+            recurse(i + 1, cur)
+            return
+
+        recurse(0, [])
+        return [list(x) for x in res]
+                
