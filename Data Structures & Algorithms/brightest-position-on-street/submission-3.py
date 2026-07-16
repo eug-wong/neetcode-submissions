49c8e4d20dc094e9712f22356c1eabaa1efab6f8
Eugene Wong <110069795+eug-wong@users.noreply.github.com>
2026-07-16 13:51:17 -0500

    Add: brightest-position-on-street - submission-3

---
+++ Data Structures & Algorithms/brightest-position-on-street/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def brightestPosition(self, lights: List[List[int]]) -> int:
+        # insight: we only care about checking the end points of each light interval
+        # if two lights overlap at some index, then it may or may not overlap within the interval
+        # so optimum is always at edge of intervals
+
+        brightness = defaultdict(int)
+        for pos, ran in lights:
+            brightness[pos - ran] += 1
+            brightness[pos + ran + 1] -= 1
+        
+        # sweep line
+        # end points of interval indicates "change before + after"
+        # if we sweep a line over the interval, we can keep track of the current brightness
+        # then just keep track of best so far and return
+
+        res = [0, 0]
+        cur = 0
+        for i, v in sorted(brightness.items()):
+            cur += v
+            if cur > res[1]:
+                res = [i, cur]
+        
+        return res[0]
