a7c71055edfcda6a89523abfc410b7c00c71298e
Eugene Wong <110069795+eug-wong@users.noreply.github.com>
2026-08-27 07:49:30 -0500

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        # recursion relation is R(n) = R(n - 1) + R(n - 2)
+        # base cases are n = 0, n = 1, n = 2
+        memo = {0: 0, 1: 1, 2: 2}
+        def recurse(n):
+            if n in memo:
+                return memo[n]
+            
+            memo[n] = recurse(n - 1) + recurse(n - 2)
+            return memo[n]
+        
+        return recurse(n)
